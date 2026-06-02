75ef899892b5bc1abb04cbbe15a255fe94007996
leandrohendrix <leandrotully@gmail.com>
2026-06-02 06:50:45 -0300

    Callbacks

--- Sintaxe/5-Callback/1.js
+++ Sintaxe/5-Callback/1.js
@@ -2,13 +2,34 @@ console.clear()
 
 // Callback é uma função passada como argumento para outra função ( Exemplo básico (síncrono) )
 
-function welcome(name) {
-  console.log('Olá, ' + name);
+/*
+  A tradução literal de callback é "retorno de chamada".
+
+  Callback é uma função passada como argumento para outra função, para que seja executada em um momento específico, 
+  normalmente após a conclusão de alguma tarefa. Em operações assíncronas, o callback é executado quando a operação termina.
+
+  Esse exemplo mostra exatamente por que callbacks são tão utilizados em operações assíncronas:
+
+  - A função principal inicia uma tarefa demorada.
+  - O JavaScript não fica bloqueado esperando.
+  - Quando a tarefa termina, o callback é executado. 
+  
+*/
+
+function prepararPizza(sabor, callback) {
+  console.log(`Preparando pizza de ${sabor}...`);
+
+  setTimeout(() => {
+    console.log(`Pizza de ${sabor} pronta!`);
+    callback(sabor); // Nesse ponto, executamos o callback (buscarPizza)
+  },1000);
+  
 }
 
-function handlerUser(callback) {
-  const name = 'Leandro';
-  callback(name);
-}
+function buscarPizza(sabor){
+  console.log(`Cliente, receba a pizza de ${sabor}!`);
+};
 
-handlerUser(welcome);
+prepararPizza('marguerita', buscarPizza);
+
+console.log('ENquanto pizza é preparada, você pode fazer outras coisas...');
--- Sintaxe/5-Callback/2.js
+++ Sintaxe/5-Callback/2.js
@@ -1,12 +1,24 @@
 console.clear()
 
-// CallBack inline
-function handlerUser(callback) {
-  const name = 'Leandro';
-  callback(name);
+// Callback é uma função passada como argumento para outra função ( Exemplo básico (síncrono) )
+
+/*
+  A tradução literal de callback é "retorno de chamada".
+  No contexto de programação, um callback é uma função que é passada como argumento para outra função.
+
+  E um passo a fazer quando a função principal termina de executar. Quando prepararPizza termina de executar, 
+  ele chama a função buscarPizza (que é o callback).
+
+  Nessa operação temos um exemplo de callback síncrono.
+*/
+
+function prepararPizza(sabor, callback) {
+  console.log(`Preparando pizza de ${sabor}...`);
+  callback(sabor); // Nesse ponto, executamos o callback (buscarPizza)
 }
 
-handlerUser((name) => {
-  console.log('Olá, ' + name);
-});
+function buscarPizza(sabor){
+  console.log(`Pizza de ${sabor} pronta!`);
+};
 
+prepararPizza('marguerita', buscarPizza);
