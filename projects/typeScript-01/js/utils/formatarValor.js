293dbf2c94ba4fc3719784c5a01136eb16816a42
leandrohendrix <leandrotully@gmail.com>
2026-06-23 18:16:49 -0300

    add type em aplicarMascaraValor

--- projects/typeScript-01/js/utils/formatarValor.js
+++ projects/typeScript-01/js/utils/formatarValor.js
@@ -1,26 +1,23 @@
 "use strict";
-
 function formatarValorMoeda(valor) {
-    const apenasDigitos = String(valor).replace(/\D/g, '');
-    const centavos = Number(apenasDigitos || 0);
-    const numero = centavos / 100;
-
+    var apenasDigitos = String(valor).replace(/\D/g, '');
+    var centavos = Number(apenasDigitos || 0);
+    var numero = centavos / 100;
     return numero.toLocaleString('pt-BR', {
         minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
+        maximumFractionDigits: 2
     });
 }
-
 function parseValorMoeda(valorFormatado) {
     if (!valorFormatado) {
         return 0;
     }
-
     return Number(valorFormatado.replace(/\./g, '').replace(',', '.'));
 }
-
 function aplicarMascaraValor(input) {
-    input.addEventListener('input', () => {
+    if (!input)
+        return;
+    input.addEventListener('input', function () {
         input.value = formatarValorMoeda(input.value);
     });
 }
---
+++ projects/typeScript-01/js/utils/formatarValor.ts
@@ -0,0 +1,27 @@
+"use strict";
+
+function formatarValorMoeda(valor: string): string {
+    const apenasDigitos = String(valor).replace(/\D/g, '');
+    const centavos = Number(apenasDigitos || 0);
+    const numero = centavos / 100;
+
+    return numero.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+}
+
+function parseValorMoeda(valorFormatado?: string): number {
+    if (!valorFormatado) {
+        return 0;
+    }
+
+    return Number(valorFormatado.replace(/\./g, '').replace(',', '.'));
+}
+
+function aplicarMascaraValor(input: HTMLInputElement): void {
+    if (!input) return;
+    input.addEventListener('input', () => {
+        input.value = formatarValorMoeda(input.value as string);
+    });
+}
